6456b9940e9dc0e4d3d375effd6e7db83a90e82d
Hellen Luciana <hellenluciana.dev@hotmail.com>
2024-09-14 20:43:53 -0300

    2 - Verificação String

---
+++ VerificacaoString/VerificacaoString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace VerificacaoString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var caracteres = "Miguel Angelo";
+
+            MatchCollection matches = Regex.Matches(caracteres.ToUpper(), "A");
+
+            if (matches.Count > 0)
+            {
+                Console.WriteLine($"Existe a letra ‘a’ ou 'A'.\nA quantidade de vezes em que ela aparece é {matches.Count}");
+            }
+            else
+            {
+                Console.WriteLine($"Não existe a letra ‘a’ ou 'A'.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
