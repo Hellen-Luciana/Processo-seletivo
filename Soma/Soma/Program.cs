cbf3a46cc5e8ea9896c9ed87d943d20f4b3d6510
Hellen Luciana <hellenluciana.dev@hotmail.com>
2024-09-14 20:59:46 -0300

    3 - Soma

---
+++ Soma/Soma/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Soma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int INDICE = 12, SOMA = 0, K = 1;
+            while (K < INDICE)
+            {
+                K = K + 1; 
+                SOMA = SOMA + K;
+            }
+
+            Console.WriteLine("O valor da variável SOMA é: " + SOMA);
+        }
+    }
+}
