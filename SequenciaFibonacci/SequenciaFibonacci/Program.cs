3aa9d6ebec5244c218a1d5046bbd899f55f13db1
Hellen Luciana <hellenluciana.dev@hotmail.com>
2024-09-14 20:02:01 -0300

    1 - Sequência de Fibonacci

---
+++ SequenciaFibonacci/SequenciaFibonacci/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SequenciaFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long f = 0;
+            long anterior = 0;
+            long atual = 1;
+            long valor = 10;
+
+            while (true) {
+                f = anterior + atual;
+                anterior = atual;
+                atual = f;
+
+                if (f >= valor)
+                {
+                    break;
+                }
+            }
+
+
+            Console.WriteLine("pertence a sequencia Fibonacci ?");
+
+            if (f == valor)
+            {
+                Console.WriteLine("pertence");
+
+            }
+            else
+            {
+                Console.WriteLine("não pertence");
+            }
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
